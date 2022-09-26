7bdcd2045a35b40d9848ca11578cb2e1390b4e33
sivakoduru <112242473+sivakoduru@users.noreply.github.com>
2022-09-26 13:25:31 +0530

    Create Hollow Square Dollar Pattern.py

---
+++ Hollow Square Dollar Pattern.py
@@ -0,0 +1,11 @@
+side=int(input("enter any side of a square:"))
+ch=input("enter any character :")
+
+print("hollow square dollar pattern")
+for i in range(side):
+    for j in range(side):
+        if (i==0 or i==side-1 or j==0 or j==side-1):
+            print('%c'%ch,end=' ')
+        else:
+            print(' ',end=' ')
+    print()
