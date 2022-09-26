d614011d5e2f12389d2744c34864d4b6b6545485
sivakoduru <112242473+sivakoduru@users.noreply.github.com>
2022-09-26 10:29:23 +0530

    Create Create Prime & Composite.py

---
+++ Create Prime & Composite.py
@@ -0,0 +1,17 @@
+a()
+b=int(input("enter the number of elements"))
+try:
+    for i in range:
+        print("enter the number")
+        for i in range(d):
+if(d<=0):
+       print("enter the positive number")
+
+    p=0
+    c=0
+    c+1
+    for j in range :
+        print("composite",c)
+        print("prime",p)
+except valueError:
+    ("Enter the input as positive integer")
