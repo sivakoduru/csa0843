2682491b4ffb0ccaf0c4413cd3fe3cceaf6e78e9
sivakoduru <112242473+sivakoduru@users.noreply.github.com>
2022-09-27 13:44:15 +0530

    Update perfect square.py

--- perfect square.py
+++ perfect square.py
@@ -1,19 +1,15 @@
-try:
-    n=int(input("N---> "))
-    if(n<=0):
-        print("ZERO AND NEGATIVE IS NOT ALLOWDED")
-        exit()
-    else:
-        count=0
-        a=1
-        while(count<n):
-            sum=0
-            for i in range(1,(a//2)+1):
-                if(a%i==0):
-                    sum=sum+i
-            if(sum==a):
-                print(a,end=" ")
-                count+=1
-            a+=1
-except ValueError:
-    print("FLOAT VALUES ARE NOT ALLOWDED")
+def getMinSquares(n):
+	if n <= 3:
+		return n;
+	res = n
+	for x in range(1, n + 1):
+		temp = x * x;
+		if temp > n:
+			break
+		else:
+			res = min(res, 1 + getMinSquares(n
+								- temp))
+	return res;
+print("enter the value of s=")
+s=int(input())
+print(getMinSquares(s))
