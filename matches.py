6f108321b983df7ede0910eb1ea76999c107ee69
sivakoduru <112242473+sivakoduru@users.noreply.github.com>
2022-09-29 09:13:09 +0530

    Create matches.py

---
+++ matches.py
@@ -0,0 +1,18 @@
+def diff_position(string1,string2):
+    a=string1.replace(' ','')
+    b=string2.replace(' ','')
+    if len(a)<len(b):
+        length=len(a)
+    else:
+        length=len(b)
+    count=0
+    for i in range(length):
+        if a[i]!=b[i]:
+            count=count+1
+    print(length-count)
+print("enter the name of the string1 :")
+string1=str(input())
+print("enter the name of the string2 :")
+string2=str(input())
+print("The Matches between the two strings are:")
+diff_position(string1,string2)
